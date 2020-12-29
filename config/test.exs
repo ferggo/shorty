06963ef251bb2fc60791998a8c2b3da0f416689c
Greg Mefford <greg@gregmefford.com>
2020-12-29 17:38:16 -0500

    Add an almost-working Wallaby integration test

--- config/test.exs
+++ config/test.exs
@@ -3,6 +3,6 @@ use Mix.Config
 # Configure your database
 config :shorty, Shorty.Repo, pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :shorty, ShortyWeb.Endpoint, server: false
+config :shorty, ShortyWeb.Endpoint, server: true
+
+config :wallaby, otp_app: :shorty
--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Shorty.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:vapor, "~> 0.10.0"}
+      {:vapor, "~> 0.10.0"},
+      {:wallaby, "~> 0.28.0", runtime: false, only: :test}
     ]
   end
 
